f691e1fe6439c0bb06eafd35d37983586a7b12f2
rosspod612 <rosspod612@tamu.edu>
2020-09-21 11:36:50 -0500

    Created index and new (C of CRUD)

---
+++ app/helpers/iise_budget_tracker_helper.rb
@@ -0,0 +1,2 @@
+module IiseBudgetTrackerHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+	
+	#root route
+	root 'iise_budget_tracker#index'
+
+  resources :iise_budget_tracker do
+		member do
+			get :delete
+		end
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
