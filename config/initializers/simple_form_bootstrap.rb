cb9b6d103bd3a494fdd5e885a0074c5f2c551488
rosspod612 <rosspod612@tamu.edu>
2020-09-22 18:57:58 -0500

    Set up DB relations and sub form field and made item migration file

---
+++ config/initializers/simple_form_bootstrap.rb
@@ -0,0 +1,11 @@
+
+#config.wrappers :dynamic, tag: 'div', class: 'form-group' do |b|
+#  b.use :html5
+#  b.wrapper tag: 'div', html: {id: "fieldsetContainer"} do |d|
+#    d.wrapper tag: 'fieldset', html: { id: "0"} do |f|
+#      f.wrapper tag: 'div', class: "form-group" do |dd|
+#        dd.use :label
+#        dd.use :input, class: "form-control"
+#      end
+#    end
+#  end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
 	#root route
 	root 'iise_budget_tracker#index'
 
-  resources :iise_budget_tracker do
+	resources :iise_budget_tracker do
 		member do
 			get :delete
 		end
 	end
+
+	get 'iise_budget_tracker/new_item' => 'iise_budget_tracker#new_item', :as => :new_item
+	
+	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
