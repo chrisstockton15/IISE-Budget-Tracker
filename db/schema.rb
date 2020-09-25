98d8d730c2280c58f652bb8b4a06194449d77f87
wilman-01 <wilmangabriel2@gmail.com>
2020-09-25 06:21:07 -0500

    Deleting unused migration file

--- db/schema.rb
+++ db/schema.rb
@@ -35,10 +35,6 @@ ActiveRecord::Schema.define(version: 20200925043338) do
     t.binary "receiptPicture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.integer "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   create_table "items", force: :cascade do |t|
