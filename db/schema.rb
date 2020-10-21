b1981a7ccc3484336be6e53174875e16c8588204
Eric Ma <me982@tamu.edu>
2020-10-20 19:18:52 -0500

    admin setup, not complete yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_164551) do
+ActiveRecord::Schema.define(version: 2020_10_20_220712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,10 +56,6 @@ ActiveRecord::Schema.define(version: 2020_10_15_164551) do
     t.binary "receiptPicture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.bigint "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   create_table "items", force: :cascade do |t|
@@ -96,6 +92,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_164551) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
